4b555b02c2275aa9a52cdb19e639553693c2d345
Leosandy <leosandy595@gmail.com>
2021-10-29 07:11:12 +0700

    feat:add login,register,ss

---
+++ app/src/main/java/com/example/projectportofolio/signup.java
@@ -0,0 +1,136 @@
+package com.example.projectportofolio;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class signup extends AppCompatActivity {
+
+    EditText username;
+    EditText email;
+    EditText password;
+    EditText phone;
+    Button btn;
+    TextView signin_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        getSupportActionBar().hide();
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+
+        username =findViewById(R.id.signup_idusertext);
+        email = findViewById(R.id.signup_idemail);
+        password = findViewById(R.id.signup_idpassword);
+        phone = findViewById(R.id.signup_idphone);
+        btn = findViewById(R.id.signup_buttonregister);
+        signin_button = findViewById(R.id.signin_button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (UserManager.getInstance() != null) {
+                    Toast.makeText(signup.this, "User already logged in!", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(signup.this, MainActivity.class));
+                    finish();
+                    return;
+                }
+
+                if (username.getText() == null || username.getText().toString().isEmpty()) {
+                    Toast.makeText(signup.this, "Username cannot be empty!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (email.getText() == null || email.getText().toString().isEmpty()) {
+                    Toast.makeText(signup.this, "Email cannot be empty!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (password.getText() == null || password.getText().toString().isEmpty()) {
+                    Toast.makeText(signup.this, "Password cannot be empty!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (phone.getText() == null || phone.getText().toString().isEmpty()) {
+                    Toast.makeText(signup.this, "Phone cannot be empty!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Task<QuerySnapshot> dataUsers = database.collection("users").get();
+
+                dataUsers.addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot target = null;
+
+                            for (QueryDocumentSnapshot documentSnapshot : task.getResult()) {
+                                if (username.getText().toString().equals(documentSnapshot.getData().get("username"))) {
+                                    target = documentSnapshot;
+                                    break;
+                                }
+                            }
+
+                            if (target != null) {
+                                Toast.makeText(signup.this, "User already exist!", Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+
+                            Map<String, Object> map = new HashMap<>();
+                            map.put("email", email.getText().toString());
+                            map.put("username", username.getText().toString());
+                            map.put("password", password.getText().toString());
+                            map.put("phone", phone.getText().toString());
+                            Task<DocumentReference> documentReferenceTask = database.collection("users").add(map);
+
+                            documentReferenceTask.addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                                @Override
+                                public void onComplete(@NonNull Task<DocumentReference> task) {
+                                    if (task.isSuccessful()) {
+                                        task.getResult().get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                                if (task.isSuccessful()) {
+                                                    UserManager.setInstance(task.getResult());
+                                                    Toast.makeText(signup.this, "User Created", Toast.LENGTH_SHORT).show();
+                                                    Intent intent = new Intent(signup.this,MainActivity.class);
+                                                    startActivity(intent);
+                                                } else  {
+                                                    Toast.makeText(signup.this, "User Created but failed to fetch user from database", Toast.LENGTH_SHORT).show();
+                                                }
+                                            }
+                                        });
+                                    } else  {
+                                        Toast.makeText(signup.this, "Failed to Create User", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                        }
+                    }
+                });
+            }
+        });
+
+        signin_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(signup.this, login.class));
+            }
+        });
+
+    }
+}
